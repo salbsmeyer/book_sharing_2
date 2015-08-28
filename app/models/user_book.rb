41111147152d4a70041d6f1cc3fb569ded99d9bc
salbsmeyer <sophiealbsmeyer@Sophies-MacBook-Pro.local>
2015-08-28 09:40:14 +0200

    SAVE

--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -3,7 +3,7 @@ class UserBook < ActiveRecord::Base
   belongs_to :user
   has_many :borrows
   has_many :borrowers, :through => :borrows
-
+  has_many :friendships, :through => :borrows
 
   def last_borrower
     borrowers.order("borrow_date").last
